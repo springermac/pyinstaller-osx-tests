db71cfa6e26529883cdfcacab37e2a944b244fe8
Jonathan Springer <springermac@gmail.com>
2016-05-30 14:17:28 -0400

    Better list

--- utils.sh
+++ utils.sh
@@ -86,13 +86,13 @@ function install_dependencies {
 function prep_cache {
     df -h
     if [ $SOURCE = macports ]; then
+        ls -R /opt/local/var/macports
         sudo rm -rf $HOME/macports_cache
         sudo rm -rf /opt/local/var/macports/software/software
         mkdir $HOME/macports_cache
         #sudo port clean --work --logs --archive installed
         if [ -d "/opt/local/var/macports/software" ]; then
             sudo mkdir $HOME/macports_cache/software
-            ls -R /opt/local/var/macports/incoming
             sudo mv /opt/local/var/macports/software $HOME/macports_cache/software
             #sudo mv /opt/local/var/macports/incoming $HOME/macports_cache/incoming
         fi
