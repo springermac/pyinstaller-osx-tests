14e1c895bc31d9dfd1dfe304f214ea648ec124f9
Jonathan Springer <springermac@gmail.com>
2016-12-18 19:31:54 -0500

    Try activate first then install

--- utils.sh
+++ utils.sh
@@ -3,10 +3,10 @@
 
 function port_install {
     PORT=$1
-    sudo port -vb install $PORT | cat
-#    if [ $? -ne 0 ]; then
-#        sudo port -vp install $PORT | cat
-#    fi
+    sudo port -vb activate $PORT | cat
+    if [ $? -ne 0 ]; then
+        sudo port -vp install $PORT | cat
+    fi
 }
 
 function install_dependencies {
