5e39629d9d21ad463ccc3c1d00e9f96282f4f6da
Jonathan Springer <springermac@gmail.com>
2016-12-23 21:22:59 -0500

    Disable more long running builds

--- utils.sh
+++ utils.sh
@@ -58,9 +58,9 @@ function install_dependencies {
             brew install --build-bottle matplotlib
             brew install --build-bottle wxpython
             brew install --build-bottle enchant --with-python
-            brew install --build-bottle pyqt
             brew install --build-bottle qt
-            brew install --build-bottle qt5
+#            brew install --build-bottle pyqt
+#            travis_wait brew install --build-bottle qt5
 #            travis_wait brew install --build-bottle pyqt5 --with-python --without-python3
             #travis_wait brew install --build-bottle pyside Takes to long
             brew install --build-bottle gst-python
@@ -69,7 +69,7 @@ function install_dependencies {
             brew install --build-bottle numpy --with-python3 --without-python
             brew install --build-bottle Pillow --with-python3 --without-python
             brew install --build-bottle matplotlib --with-python3 --without-python
-            brew install --build-bottle qt
+#            travis_wait brew install --build-bottle qt
 #            travis_wait brew install --build-bottle pyqt --with-python3 --without-python
             brew install --build-bottle qt5
 #            travis_wait brew install --build-bottle pyqt5
