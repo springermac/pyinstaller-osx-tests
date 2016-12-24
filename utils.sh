d80520d72a547d3690d067965925c3bd9f01286a
Jonathan Springer <springermac@gmail.com>
2016-12-23 22:26:22 -0500

    Try not building bottles

--- utils.sh
+++ utils.sh
@@ -58,9 +58,9 @@ function install_dependencies {
             brew install --build-bottle matplotlib
             brew install --build-bottle wxpython
             brew install --build-bottle enchant --with-python
-#            travis_wait brew install --build-bottle qt
+            brew install qt
 #            brew install --build-bottle pyqt
-#            travis_wait brew install --build-bottle qt5
+            brew install qt5
 #            brew install --build-bottle pyqt5 --with-python --without-python3
             #travis_wait brew install --build-bottle pyside Takes to long
             brew install --build-bottle gst-python
@@ -69,9 +69,9 @@ function install_dependencies {
             brew install --build-bottle numpy --with-python3 --without-python
             brew install --build-bottle Pillow --with-python3 --without-python
             brew install --build-bottle matplotlib --with-python3 --without-python
-#            travis_wait brew install --build-bottle qt
+            brew install qt
 #            brew install --build-bottle pyqt --with-python3 --without-python
-#            travis_wait brew install --build-bottle qt5
+            brew install qt5
 #            install --build-bottle pyqt5
             #travis_wait brew install --build-bottle pyside --with-python3 --without-python Takes to long
             brew install --build-bottle pygobject3 --with-python3 --without-python
@@ -116,7 +116,7 @@ function register_cache {
 
 function prep_cache {
     df -h
-    find $HOME/.pip-accel/ -name 'execnet*' -delete
+    find $HOME/.pip-accel/ -name 'execnet*' -print -delete
     if [ $SOURCE = macports ]; then
         ls -R /opt/local/var/macports/distfiles
         ls -R /opt/local/var/macports/incoming
