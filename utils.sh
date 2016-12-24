bde74d3db8b3a88349e877b8d286572218ba068d
Jonathan Springer <springermac@gmail.com>
2016-12-23 19:40:10 -0500

    Find execnet

--- utils.sh
+++ utils.sh
@@ -112,6 +112,7 @@ function register_cache {
 
 function prep_cache {
     df -h
+    find $HOME/.pip-accel/ -name 'execnet*' -print
     rm -rf $HOME/Library/Caches/pip/http/
     if [ $SOURCE = macports ]; then
         ls -R /opt/local/var/macports/distfiles
