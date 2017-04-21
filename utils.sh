ce5402f33142b739ceefc7d2b273a24c3f37ba24
Jonathan Springer <springermac@gmail.com>
2017-04-21 17:31:17 -0400

    First step to serve archives

--- utils.sh
+++ utils.sh
@@ -89,6 +89,25 @@ function install_dependencies {
 function register_cache {
     if [ -d "$HOME/macports_cache" ]; then
         ls -R $HOME/macports_cache
+        sudo port install -N lighttpd | cat
+        echo "server.document-root = "$HOME/macports_cache/software/"
+
+server.username  = "_www"
+server.groupname = "_www"
+
+server.port = 6227
+
+dir-listing.activate = "enable"
+
+mimetype.assign = (
+    ".tbz2"     => "application/x-bzip-compressed-tar",
+    ".rmd160"   => "text/binary",
+
+    # make the default mime type application/octet-stream.
+    ""          => "application/octet-stream",
+)" > $HOME/macports_cache/macports-archives-lighttpd.conf
+        cat $HOME/macports_cache/macports-archives-lighttpd.conf
+#        /opt/local/sbin/lighttpd -D -f $HOME/macports_cache/macports-archives-lighttpd.conf
     fi
     travis_fold start distfiles
     if [ -d "$HOME/macports_cache/distfiles" ]; then
@@ -97,37 +116,62 @@ function register_cache {
         ls -R /opt/local/var/macports/distfiles
     fi
     travis_fold end distfiles
-    travis_fold start incoming
-    if [ -d "$HOME/macports_cache/incoming" ]; then
-        sudo mkdir -p /opt/local/var/macports/incoming
-        sudo rsync -r --remove-source-files $HOME/macports_cache/incoming /opt/local/var/macports/
-        ls -R /opt/local/var/macports/incoming
-    fi
-    travis_fold end incoming
-    travis_fold start software
-    if [ -d "$HOME/macports_cache/software" ]; then
-        sudo mkdir -p /opt/local/var/macports/software
-        sudo rsync -r --remove-source-files $HOME/macports_cache/software /opt/local/var/macports/
-        ls -R /opt/local/var/macports/software
-    fi
-    travis_fold end software
 }
 
 function prep_cache {
     df -h
     if [ $SOURCE = macports ]; then
+        if [ ! -a "$HOME/macports_cache/local-privkey.pem" ]; then
+            openssl genrsa -out $HOME/macports_cache/local-privkey.pem 2048
+        fi
+        if [ ! -a "$HOME/macports_cache/local-pubkey.pem" ]; then
+            openssl rsa -in $HOME/macports_cache/local-privkey.pem -pubout -out $HOME/macports_cache/local-pubkey.pem
+        fi
+        sign_archives
         ls -R /opt/local/var/macports/distfiles
-        ls -R /opt/local/var/macports/incoming
         ls -R /opt/local/var/macports/software
-        sudo rm -rf $HOME/macports_cache
+        sudo rm -rf $HOME/macports_cache/distfiles
+        sudo rm -rf $HOME/macports_cache/software
         mkdir $HOME/macports_cache
         #sudo port clean --work --logs --archive installed
         sudo mv /opt/local/var/macports/distfiles $HOME/macports_cache/distfiles
-        sudo mv /opt/local/var/macports/incoming $HOME/macports_cache/incoming
         sudo mv /opt/local/var/macports/software $HOME/macports_cache/software
     fi
     if [ $SOURCE = homebrew ]; then
         brew cleanup -s
     fi
     df -h
+}
+
+function sign_archives {
+PRIVKEY="$HOME/macports_cache/local-privkey.pem"
+PUBKEY="$HOME/macports_cache//local-pubkey.pem"
+SOFTWARE="/opt/local/var/macports/software"
+
+# First, clear out any outdated signatures
+for SIGNATURE in "$SOFTWARE"/*/*.rmd160
+do
+    ARCHIVE_DIR="$(dirname "$SIGNATURE")"
+    ARCHIVE="$(basename "$SIGNATURE" .rmd160)"
+
+    if [ "$SIGNATURE" -ot "$ARCHIVE_DIR"/"$ARCHIVE" -o ! -f "$ARCHIVE_DIR"/"$ARCHIVE" ]
+    then
+        /bin/echo removing outdated signature: "$SIGNATURE"
+        /bin/rm -f "$SIGNATURE"
+    fi
+done
+
+# Now, find every archive that doesn't have a signature
+for ARCHIVE in "$SOFTWARE"/*/*.{tbz2,tgz,tar,tbz,tlz,txz,xar,zip,cpgz,cpio}
+do
+    PORTNAME="$(basename "$(dirname "$ARCHIVE")")"
+    ANAME="$(basename "$ARCHIVE")"
+
+    if [ "$ARCHIVE" -nt "$ARCHIVE".rmd160 ]
+    then
+        /bin/echo -n signing archive: "$ANAME "
+        /usr/bin/openssl dgst -ripemd160 -sign "$PRIVKEY" -out "$ARCHIVE".rmd160 "$ARCHIVE"
+        /usr/bin/openssl dgst -ripemd160 -verify "$PUBKEY" -signature "$ARCHIVE".rmd160 "$ARCHIVE"
+    fi
+done
 }
