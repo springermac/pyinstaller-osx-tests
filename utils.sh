9c5198d9b1f74b18aefbde83810e7f3e6d318b58
Jonathan Springer <springermac@gmail.com>
2018-04-09 22:38:20 -0400

    Remove pillow

--- utils.sh
+++ utils.sh
@@ -52,7 +52,7 @@ function install_dependencies {
         if [ $PYTHON_VERSION = 2 ]; then
             brew install --build-bottle python-markdown --without-python
             brew install --build-bottle numpy --without-python
-            brew install Pillow
+#            brew install Pillow
 #            brew install matplotlib
             brew install wxpython
             brew install --build-bottle enchant --with-python@2
@@ -63,7 +63,7 @@ function install_dependencies {
             brew install gst-python --with-python@2 --without-python
         elif [ $PYTHON_VERSION = 3 ]; then
             brew install --build-bottle numpy --without-python@2
-            brew install --build-bottle Pillow --without-python
+#            brew install --build-bottle Pillow --without-python
 #            brew install --build-bottle matplotlib --with-python3 --without-python
 #            brew install qt
             brew install qt5
